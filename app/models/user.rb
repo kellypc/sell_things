87e35112331c13b2ec82aaa8604cfaa7dace60ce
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2024-01-18 17:40:40 -0300

    Add Role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: { user: 0, admin: 1 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
+  validates :name, :email, presence: true
 
   has_many :courses, class_name: "course", foreign_key: "reference_id"
 end
